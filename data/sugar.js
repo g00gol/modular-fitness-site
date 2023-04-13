3b724ab757bb4f9a0d5977c9c43b8480244c8cdd
Harshdeep Aujla <125593276+haujla1@users.noreply.github.com>
2023-04-13 14:13:52 -0400

    created function to delete all sugar data for a user

--- data/sugar.js
+++ data/sugar.js
@@ -78,4 +78,41 @@ const enterSugar = async (username, sugarReading, fasting) =>
     }
 
 };
-export { enterSugar };
+
+const deleteAllSugarDataForUser = async (username) =>
+{
+    if(!username)
+    {
+        throw `Error: username not provided.`
+    }
+    if(typeof username != 'string' || username.trim().length === 0)
+    {
+        throw `Error: username must be a non empty string.`
+    }
+    username = username.trim();
+    let user
+    try 
+    {
+        let userCollection = await users();
+        user = await getByUsername(username)
+        // console.log(user)
+    }
+    catch (e)
+    {
+        console.log(e)
+        throw `error user does not exists in the users collection.`
+    }
+    try 
+    {
+        let sugarCollection = await sugar();
+        await sugarCollection.deleteMany({username: username});
+        // can retrun whatever here.
+    }
+    catch (e) 
+    {
+        console.log(e);
+        throw `Error: could not delete.`;
+    }
+
+};
+export {enterSugar, deleteAllSugarDataForUser};
