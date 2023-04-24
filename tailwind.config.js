1413cb4633d6731db94cc993f97c7a400cc4f669
g00gol <jackey2870@gmail.com>
2023-04-23 22:33:23 -0400

    made some more tailwindcss changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       colors: {
         "snow-1": "#D8DEE9",
         "snow-2": "#E5E9F0",
-        "powder-white": "#ECEFF4",
+        "snow-3": "#ECEFF4",
         "gray-1": "#4C566A",
         "gray-2": "#2E3440",
         frost: "#5E81AC",
