939e73f9a7824c2e5d0c0ec5d6c8168910c8d78c
Harshdeep Aujla <125593276+haujla1@users.noreply.github.com>
2023-04-27 14:31:09 -0400

    started working on creating API routes. need testing

---
+++ routes/api/sugar.js
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import bcrypt, { hash } from "bcrypt";
+import xss from "xss";
+
+import * as users from "../../data/users";
+import { getAllSugarObj } from "../../data/sugar"
+import session from "express-session";
+
+const sugarRouter = Router();
+sugarRouter
+  .route("/sugar")
+  .post(async (req, res) => 
+    {
+        if(!session.user)
+        {
+            return res.redirect("/error?403")
+        }
+        let user
+        try
+        {
+           user = await users.getByUsername(session.user.username) 
+        }
+        catch (e)
+        {
+            return res.redirect("/error?404")
+        }
+        try 
+        {
+            let data = await getAllSugarObj(session.user.username) 
+            return data;
+        }
+        catch (e)
+        {
+            return res.redirect("/error?404")
+        }
+
+        
+    });
+
+
+    export { sugarRouter };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import session from "express-session";
 import mainRoutes from "./main.js";
 import * as userRoutes from "./users.js";
 import dashboardRoutes from "./dashboard.js";
+import sugarRoutes from "./api/sugar.js"
 
 const constructorMethod = (app) => {
   app.use(
@@ -19,6 +20,7 @@ const constructorMethod = (app) => {
   app.use("/login", userRoutes.loginRouter);
   app.use("/signup", userRoutes.signupRouter);
   app.use("/dashboard", dashboardRoutes);
+  app.use("/api", sugarRoutes);
 
   app.use("*", (req, res) => {
     res.status(404).render("error", { errorCode: 404, errorText: "Not Found" });
