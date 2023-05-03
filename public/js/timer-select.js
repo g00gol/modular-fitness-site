f330aafe82384abaa0a374250c6dec2c88f01f99
Kyle Boberg <kwboberg@@gmail.com>
2023-05-03 13:33:52 -0400

    added stopwatch

--- public/js/timer-select.js
+++ public/js/timer-select.js
@@ -34,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     $("button.timer-select-button").on("click", function(){
         //this.id has the id for the timer
+        toggleTimerMode();
         if($(this).attr("data-title") != $('#timer-name').html()){
             clearInterval(timer);
             $('#timer-counter').attr("data-paused", "false")
@@ -44,6 +45,9 @@ document.addEventListener("DOMContentLoaded", () => {
             $('#timer-counter').attr("data-seconds", $(this).attr("data-seconds"))
             $('#countdown-progress').attr("max", $(this).attr("data-seconds"))
             $('#countdown-progress').attr("value", $(this).attr("data-seconds"))
+            $("#countdown").show();
+            $("#countdown-progress").show();
+            $("#play-pause-p").show();
         }
     });
   });
@@ -177,4 +181,100 @@ function toggleEditTimers() {
         $("#editTimersForm").off("submit").submit();
       });
     }
+  });
+
+
+
+let stopwatch = setInterval(1000000);
+
+const doStopwatch = () => {
+    clearInterval(stopwatch);
+    let time = 0
+    if($("#stopwatch-counter").attr("data-time") !=""){
+      time = parseInt($("#stopwatch-counter").attr("data-time"))
+    }
+    stopwatch = setInterval(function() {
+      time++;
+      $("#stopwatch-counter").attr("data-time",time)
+      $("#stopwatch-counter").html(formatDuration(time));
+        
+    }, 1000);
+}
+
+  function toggleStopwatchMode(){
+    $("#timer-name").html("Stopwatch");
+    $("#countdown").hide();
+    $("#countdown-progress").hide();
+    $("#play-pause-p").hide();
+    $("#timer-stopwatch-select").val("Stopwatch").change()
+    $("#stopwatch-counter").html("0:00:00")
+
+
+    $("#stopwatch-counter").attr("data-time","")
+    $("#stopwatch-counter").show();
+    $("#stopwatch-reset").show();
+    $("#stopwatch-pause-play").attr("data-state", "pause");
+    $("#stopwatch-pause-play").html("Play");
+    $("#stopwatch-pause-play").show();
+
+    return;
+    
+  }
+
+  function toggleTimerMode(){
+    $("#stopwatch-counter").hide();
+    clearInterval(stopwatch);
+    $("#timer-name").html("Select a Timer!");
+    $("#countdown").attr("data-timeLeft", "");
+    $("#countdown-progress").attr("value", "");
+    $("#countdown-progress").attr("max", "");
+    $("#timer-counter").attr("data-paused", "false")
+    $("#timer-counter").attr("data-seconds", "")
+    $("#stopwatch-pause-play").hide();
+    $("#stopwatch-reset").hide();
+
+
+    $("#timer-stopwatch-select").val("Timer").change();
+  }
+
+
+
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    // Add event listener to edit modules button
+    $("#timer-stopwatch-select").on("click", function(){
+      if($("#timer-stopwatch-select").val()=="Stopwatch" && $("#timer-name").html()!= "Stopwatch"){
+        toggleStopwatchMode();
+        return
+      }
+      if($("#timer-stopwatch-select").val()=="Timer" && $("#timer-name").html()== "Stopwatch"){
+        toggleTimerMode();
+        return
+      }
+
+    });
+
+    $("#stopwatch-pause-play").on("click", function(){
+      if($("#stopwatch-pause-play").attr("data-state")=="pause"){
+        doStopwatch();
+        $("#stopwatch-pause-play").attr("data-state", "play")
+        $("#stopwatch-pause-play").html("Pause")
+      }else{
+        clearInterval(stopwatch);
+        $("#stopwatch-pause-play").attr("data-state", "pause")
+        $("#stopwatch-pause-play").html("Play")
+      }
+    })
+
+    $("#stopwatch-reset").on("click", function(){
+      clearInterval(stopwatch);
+      $("#stopwatch-pause-play").attr("data-state", "pause")
+      $("#stopwatch-pause-play").html("Play")
+      $("#stopwatch-counter").attr("data-time", "")
+      $("#stopwatch-counter").html("0:00:00")
+
+    })
+
+
   });
