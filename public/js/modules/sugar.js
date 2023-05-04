f30382140bb413a3c8a3f7fcff75e2630c56e803
Harshdeep Aujla <125593276+haujla1@users.noreply.github.com>
2023-05-04 12:40:21 -0400

    starting client side

---
+++ public/js/modules/sugar.js
@@ -0,0 +1,14 @@
+// client side 
+const sugar_form = document.getElementById('sugarTrackerForm');
+
+if (sugar_form) 
+{
+    sugar_form.addEventListener('submit', (e) => 
+
+    {
+        e.preventDefault();
+        let sugar = document.getElementById('sugarInput').value;
+        let fast = document.getElementById('fastInput').value;
+        
+    })
+}
